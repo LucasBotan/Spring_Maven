eb8e9e74f6f16153b78661478654e7b9e61aba54
LucasBotan <83782814+LucasBotan@users.noreply.github.com>
2023-05-01 23:15:05 -0300

    Update vue.config.js
    teste deploy3

--- mba-full-05-grupo-03/frontend/vue.config.js
+++ mba-full-05-grupo-03/frontend/vue.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require('@vue/cli-service');
 
-module.exports = {
-  devServer: {
-    disableHostCheck: true,
-  },
-};
+module.exports = defineConfig({
+  transpileDependencies: true,
+  disableHostCheck: true,
+});
