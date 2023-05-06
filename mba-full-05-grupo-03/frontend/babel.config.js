30bd61369c523f0c0a4e4d8dfee418ef89696ef8
LucasBotan <83782814+LucasBotan@users.noreply.github.com>
2023-05-06 13:13:16 -0300

    Update babel.config.js

--- mba-full-05-grupo-03/frontend/babel.config.js
+++ mba-full-05-grupo-03/frontend/babel.config.js
@@ -1,5 +1,17 @@
-module.exports = {
+/*module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset',
   ],
-};
+};*/
+
+module.exports = {
+  presets: [
+    [
+      '@vue/cli-plugin-babel/preset', 
+      { 
+        targets: { esmodules: true },
+        polyfills: []
+      }
+    ]
+  ]
+}
