3bf307422709d78bd6c3f4a568e605fdf9d17d7d
LucasBotan <83782814+LucasBotan@users.noreply.github.com>
2023-05-06 11:16:31 -0300

    3

--- mba-full-05-grupo-03/frontend/vue.config.js
+++ mba-full-05-grupo-03/frontend/vue.config.js
@@ -1,8 +1,21 @@
 const { defineConfig } = require('@vue/cli-service');
 
+/*
 module.exports = defineConfig({
- // transpileDependencies: true,  
-  mode: 'production',
+  transpileDependencies: true,  
 });
+*/
+
+var webpack = require('webpack')
+
+module.exports = {
+  // ...
+  plugins: [
+    // ...
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ]
+}
 
 
