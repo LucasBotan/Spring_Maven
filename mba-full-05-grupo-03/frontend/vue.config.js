fcfdd969affc0603687b239406b77ebf4f836674
LucasBotan <83782814+LucasBotan@users.noreply.github.com>
2023-05-06 10:25:25 -0300

    teste ajuste vue.congig

--- mba-full-05-grupo-03/frontend/vue.config.js
+++ mba-full-05-grupo-03/frontend/vue.config.js
@@ -1,5 +1,15 @@
 const { defineConfig } = require('@vue/cli-service');
+doacao.up.railway.app
 
+module.exports ={
+  devServer: {
+    public: doacao.up.railway.app,
+  },
+};
+
+/*
 module.exports = defineConfig({
   transpileDependencies: true,  
 });
+*/
+
