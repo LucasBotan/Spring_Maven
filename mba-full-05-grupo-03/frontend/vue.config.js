b7533aca6e6bc3797c4710d1a3f0c830b8cafe27
LucasBotan <83782814+LucasBotan@users.noreply.github.com>
2023-05-06 11:26:08 -0300

    Update vue.config.js

--- mba-full-05-grupo-03/frontend/vue.config.js
+++ mba-full-05-grupo-03/frontend/vue.config.js
@@ -1,15 +1,9 @@
 const { defineConfig } = require('@vue/cli-service');
-var webpack = require('webpack')
 
 module.exports = defineConfig({
   transpileDependencies: true,  
   
-   plugins: [
-    // ...
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    })
-  ]
+   mode: 'production',
   
   
 });
